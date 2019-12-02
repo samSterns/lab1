b9ad68262370c189489f0539defacdd62edfc897
samSterns <sam.g.sterns@gmail.com>
2019-12-02 13:57:37 -0800

    isBoolean done

--- __tests__/types.test.js
+++ __tests__/types.test.js
@@ -1,37 +1,54 @@
 const {
-  isNumber,
-  castToNumber,
-  getCaster
+    isNumber,
+    isString,
+    isBoolean,
+    castToNumber,
+    getCaster
 } = require('../lib/types.js');
 
-describe('validator module', () => {
-  describe('basic validation', () => {
-    it('properly tells if a value is a numbers', () => {
-      expect(isNumber(3)).toBeTruthy();
-      expect(isNumber('hi')).toBeFalsy();
-      expect(isNumber([])).toBeFalsy();
-      expect(isNumber({})).toBeFalsy();
-      expect(isNumber(() => {})).toBeFalsy();
-      expect(isNumber(true)).toBeFalsy();
+describe('types function', () => {
+    describe('basic validation', () => {
+        it('properly tells if a value is a numbers', () => {
+            expect(isNumber(3)).toBeTruthy();
+            expect(isNumber('hi')).toBeFalsy();
+            expect(isNumber([])).toBeFalsy();
+            expect(isNumber({})).toBeFalsy();
+            expect(isNumber(() => {})).toBeFalsy();
+            expect(isNumber(true)).toBeFalsy();
+        });
+        it('properly tells if a value is a string', () => {
+            expect(isString('hi')).toBeTruthy();
+            expect(isString(3)).toBeFalsy();
+            expect(isString([])).toBeFalsy();
+            expect(isString({})).toBeFalsy();
+            expect(isString(() => {})).toBeFalsy();
+            expect(isString(true)).toBeFalsy();
+        });
+        it('properly tells if a value is a boolean', () => {
+            expect(isBoolean(true)).toBeTruthy();
+            expect(isBoolean([])).toBeFalsy();
+            expect(isBoolean({})).toBeFalsy();
+            expect(isBoolean(() => {})).toBeFalsy();
+        });
+      
     });
-  });
 
-  describe('casters', () => {
-    it('can cast values to a number', () => {
-      expect(castToNumber(3)).toEqual(3);
-      expect(castToNumber('3')).toEqual(3);
-      expect(castToNumber(true)).toEqual(1);
-      expect(castToNumber(false)).toEqual(0);
+    describe('casters', () => {
+        it('can cast values to a number', () => {
+            expect(castToNumber(3)).toEqual(3);
+            expect(castToNumber('3')).toEqual(3);
+            expect(castToNumber(true)).toEqual(1);
+            expect(castToNumber(false)).toEqual(0);
+        });
+
+        it('throws if value is not castable to number', () => {
+            expect(() => castToNumber('hi')).toThrowErrorMatchingSnapshot();
+            expect(() => castToNumber({})).toThrowErrorMatchingSnapshot();
+        });
     });
 
-    it('throws if value is not castable to number', () => {
-      expect(() => castToNumber('hi')).toThrowErrorMatchingSnapshot();
-      expect(() => castToNumber({})).toThrowErrorMatchingSnapshot();
+    it('can get the right caster', () => {
+        expect(getCaster(Number)).toEqual(castToNumber);
+        expect(getCaster(Promise)).toBeNull();
     });
-  });
-
-  it('can get the right caster', () => {
-    expect(getCaster(Number)).toEqual(castToNumber);
-    expect(getCaster(Promise)).toBeNull();
-  });
 });
