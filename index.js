6962600ac98efba409a5450a316f1493343627e8
samSterns <sam.g.sterns@gmail.com>
2019-12-02 16:05:59 -0800

    updated index js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-const { isNumber } = require('./lib/types.js');
+const { isNumber, isString, isBoolean, isObject, isFunction, isArray } = require('./lib/types.js');
+
 
 console.log(isNumber('3'));
+console.log(isString('3'));
+console.log(isBoolean('3'));
+console.log(isFunction('3'));
+console.log(isObject('3'));
+console.log(isArray('3'));
